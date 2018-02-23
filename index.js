314541c314876512051a5609ea457bacd7cbb19a
Levirre <companion.mail@gmail.com>
2018-02-22 19:19:12 -0800

    new home

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
- $(document).ready(function() {
-    $('#vertical').lightSlider({
-      gallery:true,
-      item:1,
-      vertical:true,
-      verticalHeight:295,
-      vThumbWidth:50,
-      thumbItem:8,
-      thumbMargin:4,
-      slideMargin:0
-    });  
-  });
-
+$('.dropdown_portfolio').on('click', () => {
+  $('.works_list').toggle('fast');
+});
